ff3e68a6dca9e5ef71071bd1973033c47c20afb2
Sadek-21 <mohamed20000327@gmail.com>
2024-01-16 14:26:32 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,16 +12,21 @@ char *str_concat(char *s1, char *s2)
 	int i, ci;
 
 	if (s1 == NULL)
+
 		s1 = "";
 
 	if (s2 == NULL)
+
 		s2 = "";
+
 		i = ci = 0;
 
 	while (s1[i] != '\0')
+
 		i++;
 
 	while (s2[ci] != '\0')
+
 		ci++;
 
 	conct = malloc(sizeof(char) * (i + ci + 1));
@@ -33,13 +38,18 @@ char *str_concat(char *s1, char *s2)
 	i = ci = 0;
 
 	while (s1[i] != '\0')
+
 	{
 		conct[i] = s1[i];
+
 		i++;
 	}
+
 	while (s2[ci] != '\0')
+
 	{
 		conct[i] = s2[ci];
+
 		i++, ci++;
 	}
 
